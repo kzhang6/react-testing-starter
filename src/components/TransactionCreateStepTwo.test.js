e50acefc4bb077e45707d01e72acb1a0c6f5b51f
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-18 14:37:08 -0700

    Fix Formik warning Warning: An update to Formik inside a test was not wrapped in act(...).

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo.tsx";
 
-test("on initial render, the pay button is disabled", () => {
+test("on initial render, the pay button is disabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
-  //   screen.debug();
-  //   screen.getByRole("");
-  expect(screen.getByRole("button", { name: /pay/i })).toBeEnabled();
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 });
