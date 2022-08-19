fa2587dbbdb9eafe7a9816bbdc30eacaee5be210
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-18 22:24:16 -0700

    log in and check balance

--- cypress/e2e/integration/payment_spec.cy.js
+++ cypress/e2e/integration/payment_spec.cy.js
@@ -1,5 +1,14 @@
 describe("payment", () => {
   it("user can make payment", () => {
     cy.visit("localhost:3000", { timeout: 30000 });
+    cy.findByRole("textbox", { name: /username/i }).type("johndoe"); //equivlent to getByRole in RTL
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
+
+    let oldBalance;
+    cy.get("[data-test=sidenav-user-balance]")
+      .then(($balance) => (oldBalance = $balance.text()))
+      .then((balance) => console.log(balance));
   });
 });
