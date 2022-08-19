9b3f0372261a90d644ce55e76e61de413cf07cd2
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-18 20:46:49 -0700

    Combine unit tests into an integration test?

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,15 +2,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo.tsx";
 
-test("on initial render, the pay button is disabled", async () => {
+//Arrange, Act, Assert pattern
+test("if an amount and note is entered, the pay button becomes enabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
   // pay button disabled when page loads?
   expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
-});
-
-test("if an amount and note is entered, the pay button becomes enabled", async () => {
-  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
   userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
